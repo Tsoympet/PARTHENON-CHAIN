6161b1b2619583f10030b4b90ae8d2cdc9fb2fce
Tsoympet <tsoympet7@gmail.com>
2025-12-27 16:47:54 +0200

    Update sha256d.cpp

--- layer1-core/pow/sha256d.cpp
+++ layer1-core/pow/sha256d.cpp
@@ -1,2 +1,17 @@
-// TODO: Full implementation will be added by Codex.
-// Part of the DRACHMA blockchain project.
+#include "sha256d.h"
+#include <openssl/sha.h>
+
+Hash256 SHA256(const uint8_t* data, size_t len) {
+    Hash256 hash;
+    SHA256_CTX ctx;
+    SHA256_Init(&ctx);
+    SHA256_Update(&ctx, data, len);
+    SHA256_Final(hash.data(), &ctx);
+    return hash;
+}
+
+Hash256 SHA256d(const uint8_t* data, size_t len) {
+    auto first = SHA256(data, len);
+    return SHA256(first.data(), first.size());
+}
+
