d7b84e5d6e7ba1aec1e03c23f1c84c4851cbc142
Tsoympet <tsoympet7@gmail.com>
2025-12-27 16:46:25 +0200

    Update params.cpp

--- layer1-core/consensus/params.cpp
+++ layer1-core/consensus/params.cpp
@@ -1,2 +1,34 @@
-// TODO: Full implementation will be added by Codex.
-// Part of the DRACHMA blockchain project.
+#include "params.h"
+
+namespace consensus {
+
+static Params mainParams {
+    210000,            // halving interval
+    60,                // 60 sec block time
+    3600,              // 1 hour retarget window
+    60,                // 60 blocks
+    42000000ULL * 100000000ULL,
+    false,             // no min-difficulty blocks
+    1735689600,        // genesis time
+    0x1e0fffff,        // initial difficulty
+    0,                 // mined later
+    "DRACHMA: Proof before trust"
+};
+
+static Params testParams {
+    210000,
+    60,
+    3600,
+    60,
+    42000000ULL * 100000000ULL,
+    true,              // allow min difficulty
+    1735689600,
+    0x1f00ffff,
+    0,
+    "DRACHMA TESTNET"
+};
+
+const Params& Main()    { return mainParams; }
+const Params& Testnet() { return testParams; }
+
+} // namespace consensus
