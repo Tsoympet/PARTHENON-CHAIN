d9f22fee4f35910f4842405d96ff611744b1a0a2
Tsoympet <tsoympet7@gmail.com>
2025-12-28 19:04:36 +0200

    Update transaction.h

--- layer1-core/tx/transaction.h
+++ layer1-core/tx/transaction.h
@@ -1,37 +1,23 @@
 #pragma once
+#include "txin.h"
+#include "txout.h"
 #include <vector>
-#include <cstdint>
 #include <string>
-#include "../crypto/tagged_hash.h"
+#include <cstdint>
 
-struct OutPoint {
-    uint256 hash;
-    uint32_t index;
-};
-
-struct TxIn {
-    OutPoint prevout;
-    std::vector<uint8_t> scriptSig;
-    uint32_t sequence{0xffffffff};
-};
-
-struct TxOut {
-    uint64_t value;
-    std::vector<uint8_t> scriptPubKey;
-};
-
-struct Transaction {
-    uint32_t version{1};
+class Transaction {
+public:
+    uint32_t version;
     std::vector<TxIn> vin;
     std::vector<TxOut> vout;
-    uint32_t lockTime{0};
+    uint32_t lockTime;
 
-    uint256 GetHash() const;
+    Transaction();
+
+    bool isCoinbase() const;
+    uint64_t totalOutput() const;
+    std::string getTxID() const;
+
+    std::vector<uint8_t> serialize() const;
+    static Transaction deserialize(const std::vector<uint8_t>& data);
 };
-
-// Serialization helpers
-std::vector<uint8_t> Serialize(const Transaction& tx);
-Transaction DeserializeTransaction(const std::vector<uint8_t>& data);
-
-// Utility for tagged hash of a transaction
-uint256 TransactionHash(const Transaction& tx);
